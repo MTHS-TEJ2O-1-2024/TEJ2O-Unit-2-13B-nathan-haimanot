ba1a6b669112870d67a0d8f546524dfb0d21b9bc
nathan-haimanot <nathan.haimanot@mths.ca>
2024-10-30 17:18:23 -0400

    Made a countdown program

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Nathan Haimanot
+ * Created on: Oct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let loopCounter = 5
+let neopixelStrip: neopixel.Strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+input.onButtonPressed(Button.A, function () {
+    let loopCounter = 5
+    while (loopCounter > 0) {
+    neopixelStrip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    loopCounter -= 1
+    basic.showNumber(loopCounter)
+    basic.clearScreen()
+    }
+basic.showIcon(IconNames.Happy)
+})
